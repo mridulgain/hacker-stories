46996e1b577dbd118e5c4eeeec6eaddc91463e06
Ansh <abhijaisingh15@gmail.com>
2022-10-08 20:54:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,72 +1,90 @@
-import React from 'react';
+import './App.css';
+import * as React from 'react'
 
 const App = () => {
-  const stories = [
+  const list = [
     {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
+      id: '1',
+      name: 'Jordan Walke',
+      class: 3,
+      roll_number: 4,
+      marks: 95,
     },
     {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
+      id: '2',
+      name: 'Dan Abramov',
+      class: 2,
+      roll_number: 5,
+      marks: 97,
+    },
+  ];
+  const list2 = [
+    {
+      id: '3',
+      name: 'abhi',
+      class: 3,
+      roll_number: 4,
+      marks: 95,
+    },
+    {
+      id: '4',
+      name: 'rohan',
+      class: 2,
+      roll_number: 5,
+      marks: 97,
     },
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState('React');
+  const [count, set_count] = React.useState(0)
 
-  const handleSearch = event => {
-    setSearchTerm(event.target.value);
-  };
+  const [search, set_search] = React.useState('')
 
-  const searchedStories = stories.filter(story =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const handleSearch = (event) => {
+    set_search(event.target.value)
+  }
 
-  return (
+  return(
     <div>
-      <h1>My Hacker Stories</h1>
+      <h1 class="heading">LIST OF STUDENTS</h1>
+      <p>You clicked {count} number of times</p>
+      <button type="button" onClick={ () => set_count(count+1)}>up the count</button>
 
-      <Search search={searchTerm} onSearch={handleSearch} />
+      <input type="text" onChange={handleSearch}></input>
 
-      <hr />
-
-      <List list={searchedStories} />
+      <p>you are search for <strong><u>{search}</u></strong></p> 
+      <div>
+        <Student list={list}/>
+        <hr />
+        <Student list={list2}/>
+      </div>
+      <div>
+        
+      </div>
     </div>
-  );
-};
+  )
+}
 
-const Search = ({ search, onSearch }) => (
+const Student = (props) => {
+  // const [current_student, set_student] = React.useState(props.list)
+  // const deleteStudent = (event) => {
+  //   deleteStudent(event.target.id)
+  //   console.log(event.target.value)
+  //   props.list.pop()
+  // }
+  return(
   <div>
-    <label htmlFor="search">Search: </label>
-    <input
-      id="search"
-      type="text"
-      value={search}
-      onChange={onSearch}
-    />
+    <ul>
+      {props.list.map((item, index) => (
+        <li key={item.id}>
+          <p>Name: {item.name}</p>
+          <p>Class: {item.class}</p>
+          <p>Roll_number: {item.roll_number}</p>
+          <p>Marks: {item.marks}</p>
+          <button type="button" value={index} onClick={props.onRemove}>Remove</button>
+        </li>
+      ))}
+    </ul>
   </div>
-);
-
-const List = ({ list }) =>
-  list.map(item => <Item key={item.objectID} item={item} />);
-
-const Item = ({ item }) => (
-  <div>
-    <span>
-      <a href={item.url}>{item.title}</a>
-    </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-  </div>
-);
+)};
 
 export default App;
