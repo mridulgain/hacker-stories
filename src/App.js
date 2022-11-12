6cb52b107a963fa29126e6b6e5cdb18775dd74b4
Mridul <mridulgain@gmail.com>
2022-11-13 00:12:45 +0530

    props handling(advanced): using spread operator

--- src/App.js
+++ src/App.js
@@ -56,16 +56,16 @@ const Search = ({ search, onSearch }) => (
 );
 
 const List = ({ list }) =>
-  list.map(item => <Item key={item.objectID} item={item} />);
+  list.map(item => <Item key={item.objectID} {...item} />);
 
-const Item = ({ item }) => (
+const Item = ({ url, title, author, num_comments, points }) => (
   <div>
     <span>
-      <a href={item.url}>{item.title}</a>
+      <a href={url}>{title}</a>
     </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
+    <span>{author}</span>
+    <span>{num_comments}</span>
+    <span>{points}</span>
   </div>
 );
 
